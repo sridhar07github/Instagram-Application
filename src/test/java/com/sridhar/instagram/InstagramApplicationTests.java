096cf04f37f377014b76980a7facaf61bd0c0041
sridhar07 <sridharsenthilkumar07@gmail.com>
2024-04-12 23:19:30 +0530

    first commit

---
+++ src/test/java/com/sridhar/instagram/InstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sridhar.instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
